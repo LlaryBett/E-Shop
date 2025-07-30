752b33f0fc1eb6878c4b37a65315c1ae01b31496
@Bett <126676083+LlaryBett@users.noreply.github.com>
2025-07-30 16:16:09 +0300

    Updated adminController.js

--- server/controllers/adminController.js
+++ server/controllers/adminController.js
@@ -2,6 +2,8 @@ import User from '../models/User.js';
 import Product from '../models/Product.js';
 import Order from '../models/Order.js';
 import Category from '../models/Category.js';
+ // Get admin dashboard stats 
+
 
 //Get admin dashboard stats 
 // @route   GET /api/admin/stats
