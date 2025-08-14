508c8a111ceb9ff6653eeb10833ba2c02c29f5aa
Llary <bettllary672@gmail.com>
2025-08-14 17:38:13 +0300

    `Update API base URL in api.js`

--- client/src/services/api.js
+++ client/src/services/api.js
@@ -2,8 +2,8 @@ import axios from 'axios';
 
 const API_BASE_URL =
   import.meta.env.MODE === 'production'
-    ? 'https://your-deployed-backend.com/api' // Change this in production
-    : 'http://localhost:5000/api';
+    ? 'https://e-shop-3-2mab.onrender.com/api' // Change this in production
+    : 'https://e-shop-3-2mab.onrender.com/api';
 
 const api = axios.create({
   baseURL: API_BASE_URL,
